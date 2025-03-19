09dbc65c58d44a868151a312581b63f78ea1b837
Itstritix <jiabao.tsui@gmail.com>
2025-03-19 14:45:35 +0100

    add register route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+const bcrypt = require('bcrypt');
 const fs = require("fs");
 const path = require("path");
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('db.db');
+const saltRounds = 10;
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/api/champions", (req, res) => {
     fs.readFile("character.json", "utf8", (err, data) => {
@@ -31,6 +36,28 @@ app.get("/api/cocktails", (req, res) => {
         res.json(filteredCocktails);
     });
 });
+
+app.post("/api/user", (req, res) => {
+
+    bcrypt.hash(req.body.UserPassword, saltRounds, function(err, hash) {
+        if (err != nil) {
+            console.log(err);
+        }
+        db.run('INSERT INTO user(username, email, password) VALUES($username, $email, $password)', {
+            $username: req.body.Username,
+            $email: req.body.UserMail,
+            $password: hash,
+        });
+    });
+
+    res.redirect('/register');
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/register.html'));
+})
+
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
